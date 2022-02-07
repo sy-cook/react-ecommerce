223e5b2ecc025f2978044605350d2342cc1eb37d
sy-cook <seyoung.cook@gmail.com>
2022-02-07 15:51:21 -0800

    Add createUserProfileDocument in firebase.utils to store user data in Firebase.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component.jsx';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends Component {
   constructor() {
@@ -19,11 +19,26 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        // Use userRef to check if our database has updated at that reference with any new data.
+        // Calling .onSnapshot() method is similar to calling the .onAuthStateChanged() method.
+        // Call .data() method on documentSnapshot object to get the actual properties (as a JSON object).
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            });
+        });
+      } else {
+        // If no userAuth, set currentUser state to `null`
+        this.setState({currentUser: userAuth});
+      }
+    });
   }
 
   componentWillUnmount() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,38 @@ const config = {
   measurementId: "G-Y5JNWZBK1Q"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return; // If there is no userAuth (i.e., userAuth = `null`), return
+
+  // If userAuth exists, query firestore 
+
+  // documentReference:
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+  // documentSnapshot:
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    // If the user doesn't exist, create a new user using the data from userAuth
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  // Return userRef, because there is a chance we want to use userRef to do other things.
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
