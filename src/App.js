2bc583a6f371fc1887847b31d80cb9e95af75007
sy-cook <seyoung.cook@gmail.com>
2022-02-07 16:14:44 -0800

    Add sign-up component and update sign-in-and-sign-up component.

--- src/App.js
+++ src/App.js
@@ -33,6 +33,8 @@ class App extends Component {
                 ...snapShot.data()
               }
             });
+
+          console.log(this.state);
         });
       } else {
         // If no userAuth, set currentUser state to `null`
